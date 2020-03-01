4e9f0769025bea89e100eafc4a6f8f9e80e93513
moss-79 <mosskdori@gmail.com>
2020-03-01 03:15:27 -0600

    update

--- src/Day1/LoopPractice.java
+++ src/Day1/LoopPractice.java
@@ -4,7 +4,7 @@ public class LoopPractice {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i+=2) {
             System.out.println(" counter "+ i);
-            System.out.println("update this is a commit practice");
+            System.out.println("update this is a commit practice t");
         }
     }
 }
